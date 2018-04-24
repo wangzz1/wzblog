80117c9ee9f54587da2f5544e6fee1a798b03de1
wangzz1 <632332271@qq.com>
2018-04-25 00:03:18 +0800

    delete venv

--- manage.py
+++ manage.py
@@ -1,23 +1,14 @@
 from flask import Flask,render_template
-#from flask_script import Manager #脚本和系统分离，插入外部脚本
-#from flask_moment import Moment #时间日期扩展
-#from flask_sqlalchemy import SQLAlchemy
+from flask_script import Manager
 app = Flask(__name__)
-#manager = Manager(app)
-#moment = Moment(app)
-#db = SQLAlchemy(app)
-
-'''class User():
-    __tablename__ = 'users'
-    id = db.Column(db.Integer,primary_key=True)
-    name = db.Column(db.String(20))
-    password = db.Column(db.Integer)
-'''
+manager = Manager(app)
 @app.route('/')
 def index():
-    return render_template('base.html')
+    return render_template('index.html')
 
-
+@app.route('/login')
+def login():
+    return render_template('login.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    manager.run()

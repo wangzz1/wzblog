2f266b665d86f7c61e058d83097f09f1ea743909
wangzz1 <632332271@qq.com>
2018-04-25 22:53:32 +0800

    ...

--- manage.py
+++ manage.py
@@ -1,14 +1,39 @@
-from flask import Flask,render_template
-from flask_script import Manager
+import os
+from flask import Flask,render_template,request
+from flask_sqlalchemy import SQLAlchemy
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 app = Flask(__name__)
-manager = Manager(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = \
+    'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer,primary_key=True)
+    email = db.Column(db.String(50),unique=True,index=True)
+    password = db.Column(db.VARCHAR(200))
+class Blog(db.Model):
+    __tablename__ = 'blogs'
+    id = db.Column(db.Integer,primary_key=True)
+    title = db.Column(db.String(200))
+    article = db.Column(db.Text)
+
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/login')
+@app.route('/login',methods=['GET','POST'])
 def login():
-    return render_template('login.html')
+    if request.method == 'GET':
+        return render_template('login.html')
+    elif request.method == 'POST':
+        pass
+
 
 if __name__ == '__main__':
-    manager.run()
+    app.run(debug=True)
